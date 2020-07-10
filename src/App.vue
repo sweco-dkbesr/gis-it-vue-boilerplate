22abd92efefd7d9ee4b7778a35d9d9bedc115807
Bernhard Snizek <bernhard.snizek@sweco.dk>
2020-07-10 14:20:17 +0200

    minus avenir

--- src/App.vue
+++ src/App.vue
@@ -21,8 +21,8 @@
 </template>
 
 <style lang="scss">
+//font-family: Avenir, Helvetica, Arial, sans-serif;
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
